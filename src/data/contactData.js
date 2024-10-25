66a2f0ccd9359b639c639011d8a2220daa04323e
HugoRomz <rosalesrafael1@hotmail.com>
2024-10-25 06:37:51 -0600

    NewVersion

---
+++ src/data/contactData.js
@@ -0,0 +1,28 @@
+export const contactData = {
+  emails: [
+    {
+      email: "Didierramirezcr7@gmail.com",
+      url: "mailto:Didierramirezcr7@gmail.com",
+    },
+  ],
+  phones: [
+    {
+      display: "(754) 294-6100", // Para mostrar
+      link: "7542946100", // Para el enlace tel:
+    },
+  ],
+  socialLinks: [
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/profile.php?id=100094686463554",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/ramirez_window_cleaning/",
+    },
+    {
+      name: "Angi",
+      url: "https://www.angi.com/companylist/us/fl/fort-lauderdale/ramirez-window-cleaning-reviews-10680959.htm",
+    },
+  ],
+};
---
+++ src/data/servicesData.js
@@ -0,0 +1,36 @@
+const pathImage = "/services/";
+
+export const services = [
+  {
+    slug: "windowcleaning",
+    category: "WINDOWS",
+    title: "Windows Cleaning",
+    description:
+      "Professional window cleaning services that ensure impeccable clarity and shine. At Ramirez Window Cleaning, we bring unparalleled brilliance to your home or office with our expert window cleaning solutions. Clean windows aren’t just about the view – they’re about making a great impression and fully enjoying your space. Our skilled team uses the latest tools and techniques to deliver streak-free results, meticulously removing dirt, grime, and residues to leave your windows sparkling. \n\n Whether you have a residential property or a commercial building, our dedication to detail and excellence is unwavering. Safety is always a priority, particularly for high-rise projects, with fully trained professionals adhering to the highest safety standards. We also take pride in our environmentally friendly practices, using biodegradable and eco-friendly cleaning solutions that are safe for your family, employees, and the planet. \n\n Choose Ramirez Window Cleaning for a clear difference – because pristine windows reflect the quality and care you bring to your space.",
+    imageUrl: `${pathImage}window5.webp`,
+  },
+  {
+    slug: "sealing",
+    category: "SEALING",
+    title: "Sealing",
+    description:
+      "Superior sealing solutions to protect and preserve your surfaces. At Ramirez Window Cleaning, we specialize in top-tier sealing services, meticulously crafted to enhance the durability and beauty of your property's surfaces. We understand the vital role that quality sealing plays in maintaining both the integrity and aesthetics of your spaces, and we’re dedicated to delivering exceptional precision and care in every project.\n\n Our expert team brings extensive experience in sealing a variety of surfaces, from driveways and decks to patios, using only the highest-quality sealants. These sealants not only enhance the appearance of your surfaces but also protect them from wear, weathering, and environmental factors. Our process begins with a comprehensive assessment of each area to determine the most effective sealing approach. We thoroughly clean and prepare the surface before carefully applying the sealant to create a uniform, durable barrier that enhances both functionality and appearance.\n\n With a focus on safety and sustainability, we use eco-friendly, low-VOC sealants, ensuring protection for your family, pets, and the environment without compromising on performance. Choose Ramirez Window Cleaning for your sealing needs and experience peace of mind, knowing your property is in expert hands. Our commitment to quality and customer satisfaction means your sealed surfaces will stand the test of time, adding lasting value and beauty to your property.",
+    imageUrl: `${pathImage}sealing2.webp`,
+  },
+  {
+    slug: "roofcleaning",
+    category: "ROOF",
+    title: "Roof Cleaning",
+    description:
+      "Expert roof cleaning services to maintain and enhance your property’s appearance. At Ramirez Window Cleaning, we provide professional roof cleaning solutions designed to elevate both the appearance and longevity of your property. A clean roof goes beyond aesthetics; it preserves your property’s value and functionality. Our team is skilled in cleaning various roof types, including asphalt shingles, tiles, and metal, using advanced equipment and environmentally friendly cleaning agents to gently and effectively remove moss, algae, dirt, and debris that can lead to long-term damage.\n\n Safety is central to our approach. Our technicians are extensively trained in the latest safety protocols and equipped with specialized gear to ensure efficient and damage-free cleaning. To protect both your roof and the environment, we use only eco-friendly, non-abrasive cleaning solutions that safeguard the integrity of your roofing materials while delivering a thorough cleanse.\n\n Choosing Ramirez Window Cleaning for your roof maintenance needs not only enhances your property’s curb appeal but also extends the life of your roof, helping you avoid costly repairs or replacements down the line. Trust us to restore your roof’s original luster with meticulous care and attention.",
+    imageUrl: `${pathImage}roof3.webp`,
+  },
+  {
+    slug: "interiorandexteriorpainting",
+    category: "ROOF",
+    title: "Interior and Exterior Painting",
+    description:
+      "Painting services for elegant and durable interior and exterior finishes. At Ramirez Window Cleaning, we provide exceptional painting services that transform your spaces into masterpieces of style and sophistication. Our commitment to excellence shines in every brush stroke, with meticulous attention to detail and quality at the forefront of each project. Our team of skilled painters specializes in both interior and exterior finishes, tailoring each job to your aesthetic and functional needs.\n\n For interior painting, we focus on creating a harmonious atmosphere that reflects your personal style, from selecting the ideal color palette to achieving flawless finishes. We ensure a seamless and minimally disruptive process, allowing you to enjoy the transformation with ease. In exterior painting, our aim is not only to enhance your property’s appearance but also to protect it from the elements. We use premium-quality paints and materials that are resilient to harsh weather, ensuring long-lasting beauty and durability. \n\n Our eco-friendly approach means we use low-VOC, environmentally sustainable paints to ensure the well-being of your family and the planet. Each project is completed with a sense of environmental responsibility and craftsmanship. Choosing Ramirez Window Cleaning for your painting needs means opting for a team that values precision, professionalism, and customer satisfaction above all else. Let us bring your vision to life with colors that inspire and endure.",
+    imageUrl: `${pathImage}pinture1.webp`,
+  },
+];
